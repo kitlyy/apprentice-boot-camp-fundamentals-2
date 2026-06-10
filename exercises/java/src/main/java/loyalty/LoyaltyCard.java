e0c4fbce1b4021ad7ce26f9bc17000d3a65b6786
Kit Ly <kit.ly@autotrader.co.uk>
2026-06-10 14:45:52 +0100

    refactored for double points feature

--- exercises/java/src/main/java/loyalty/LoyaltyCard.java
+++ exercises/java/src/main/java/loyalty/LoyaltyCard.java
@@ -1,7 +1,15 @@
 package loyalty;
 
 public class LoyaltyCard {
+
+    int pointBalance = 0;
+
+
     public int getPointsFromPurchaseCost(float purchaseCost) {
-        return (int) purchaseCost;
+        if (purchaseCost <= 100) {
+            return (int) purchaseCost;
+        } else {
+            return (int) (100 + 2*(purchaseCost-100));
+        }
     }
 }
--- exercises/java/src/test/java/loyalty/LoyaltyTest.java
+++ exercises/java/src/test/java/loyalty/LoyaltyTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 public class LoyaltyTest {
     @Test
-    public void purchaseCostOf74PoundsGives74Points(){
+    public void purchaseCostOf74PoundsGives74Points() {
         //arrange
         LoyaltyCard card = new LoyaltyCard();
         int expected = 74;
@@ -14,4 +14,22 @@ public class LoyaltyTest {
         //assert
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test void purchaseCostOf124PoundsGives148Points() {
+        LoyaltyCard card = new LoyaltyCard();
+        int expected = 148;
+
+        int actual = card.getPointsFromPurchaseCost(124);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+//    @Test void purchaseOfCost74And124Gives() {
+//        LoyaltyCard card = new LoyaltyCard();
+//        int expected = 222;
+//
+//        int actual = card.getTotalPoints();
+//
+//        Assertions.assertEquals(expected, actual);
+//    }
 }
