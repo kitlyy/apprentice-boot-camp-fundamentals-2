81b0ecf31ade35409c1fb0c587760ca92d61737d
Kit Ly <kit.ly@autotrader.co.uk>
2026-06-10 16:36:11 +0100

    can remove expired point entries

--- exercises/java/src/main/java/loyalty/LoyaltyCard.java
+++ exercises/java/src/main/java/loyalty/LoyaltyCard.java
@@ -1,26 +1,57 @@
 package loyalty;
 
+import javax.naming.InsufficientResourcesException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 public class LoyaltyCard {
 
+    HashMap<Integer, Integer> listOfPoints;
     int pointBalance = 0;
 
 
-    public int getPointsFromPurchaseCost(float purchaseCost) {
+    LoyaltyCard() {
+        this(new HashMap<>());
+    }
+    LoyaltyCard(HashMap<Integer, Integer> listOfPoints) {
+        this.listOfPoints = listOfPoints;
+    }
+    int getPointsFromPurchaseCost(float purchaseCost) {
         if (purchaseCost <= 100) {
             return (int) purchaseCost;
-        } else {
-            return (int) (100 + 2*(purchaseCost-100));
         }
+        return (int) (100 + 2*(purchaseCost-100));
     }
-    
 
-    
-    
     public int getPointBalance() {
         return pointBalance;
     }
 
     public void makePurchase(int purchaseCost) {
-        pointBalance += getPointsFromPurchaseCost(purchaseCost);
+        int points = getPointsFromPurchaseCost(purchaseCost);
+        pointBalance += points;
+        listOfPoints.put(points, 365);
+        removeExpiredPoints();
+        if (pointBalance > 5000) {
+            pointBalance -= 5000;
+            System.out.println("£50 in-store gift card has been issues");
+        }
+    }
+
+    public void removeExpiredPoints() {
+        HashMap<Integer, Integer> toRemove = new HashMap<>();
+        for (Map.Entry<Integer, Integer> entry : listOfPoints.entrySet()) {
+            if (entry.getValue() < 1) {
+                toRemove.put(entry.getKey(), entry.getValue());
+            }
+        }
+        for (Map.Entry<Integer, Integer> entry : toRemove.entrySet()) {
+            listOfPoints.remove(entry.getKey());
+        }
+    }
+
+    public HashMap<Integer, Integer> getListOfPoints() {
+        return listOfPoints;
     }
 }
--- exercises/java/src/test/java/loyalty/LoyaltyTest.java
+++ exercises/java/src/test/java/loyalty/LoyaltyTest.java
@@ -3,7 +3,22 @@ package loyalty;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.util.Map.entry;
+
 public class LoyaltyTest {
+
+    /*
+    if this method becomes private, as we have tested it and we know it works,
+    we have to delete these tests because it is now private
+
+    we can make it so that it has default visibility, but then children have access to it which
+    might not be the desired behavior
+
+    where is the paper-trail of tests that confirm the method actually works??
+     */
     @Test
     public void purchaseCostOf74PoundsGives74Points() {
         //arrange
@@ -28,11 +43,41 @@ public class LoyaltyTest {
         LoyaltyCard card = new LoyaltyCard();
         int expected = 222;
 
-        //act
         card.makePurchase(74);
         card.makePurchase(124);
         int actual = card.getPointBalance();
 
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test void purchaseOf5000LeavesBalanceOf4900() {
+        LoyaltyCard card = new LoyaltyCard();
+        int expected = 4900;
+
+        card.makePurchase(5000);
+        int actual = card.getPointBalance();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test void removePointsThatHaveExpired() {
+        LoyaltyCard card = new LoyaltyCard(new HashMap<>(Map.ofEntries(
+                entry(60, 260),
+                entry(100, 40),
+                entry(5, 0),
+                entry(600, -1))));
+        HashMap<Integer, Integer> expectedList = new HashMap(Map.ofEntries(
+                entry(60, 260),
+                entry(100, 40)));
+
+        //act
+
+        card.removeExpiredPoints();
+        HashMap<Integer, Integer> actualList = card.getListOfPoints();
+
+        Assertions.assertEquals(expectedList, actualList);
+
+    }
 }
+
+
