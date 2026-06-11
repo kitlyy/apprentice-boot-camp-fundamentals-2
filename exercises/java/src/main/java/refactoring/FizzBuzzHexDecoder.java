05bdeb4b3d1d81a1b20e88f14a7457b3b31e921b
Kit Ly <kit.ly@autotrader.co.uk>
2026-06-11 13:46:51 +0100

    removed redundant method abstractions

--- exercises/java/src/main/java/refactoring/FizzBuzzHexDecoder.java
+++ exercises/java/src/main/java/refactoring/FizzBuzzHexDecoder.java
@@ -7,10 +7,6 @@ import java.nio.charset.StandardCharsets;
 
 public class FizzBuzzHexDecoder {
 
-    private final String FIZZ_HEX = "46697a7a";
-    private final String BUZZ_HEX = "42757a7a";
-
-
     private int startingIndex;
     private int fizz;
     private int buzz = 5;
@@ -24,27 +20,16 @@ public class FizzBuzzHexDecoder {
     private String getFizzBuzz(int integerInSequence) {
         fizz++;
         buzz--;
-        String result = fizz == 3 || buzz == 0 ? "" : String.valueOf(integerInSequence + 1);
-        if (fizz == 3) result += getFizz();
-        if (buzz == 0) result += getBuzz();
+        boolean isFizzBuzz = fizz == 3 || buzz == 0;
+        String result = isFizzBuzz ? "" : String.valueOf(integerInSequence + 1);
+        if (fizz == 3) {
+            fizz = 0;
+            result += "Fizz";
+        }
+        if (buzz == 0) {
+            buzz = 5;
+            result += "Buzz";
+        }
         return result;
-    }
-
-    private String getBuzz() {
-        buzz = 5;
-        try {
-            return new String(Hex.decodeHex(BUZZ_HEX), StandardCharsets.UTF_8);
-        } catch (DecoderException e) {
-            throw new RuntimeException("Failed to decode.", e);
-        }
-    }
-
-    private String getFizz() {
-        fizz = 0;
-        try {
-            return new String(Hex.decodeHex(FIZZ_HEX), StandardCharsets.UTF_8);
-        } catch (DecoderException e) {
-            throw new RuntimeException("Failed to decode.", e);
-        }
     }
 }
